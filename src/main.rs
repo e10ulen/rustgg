2f6d5e09cb11288d40a268c14880a1c815d80027
e10ulen <slcb.sena@gmail.com>
2025-03-05 11:21:52 +0900

    関数をまとめた

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,16 @@
+extern crate log;
+use log::warn;
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 fn main() {
-    //test_echo();
-    git_add();
-    git_commit();
-    git_push();
-}
-
-fn git_add() {
     println!("Git Add files.");
     let err = Command::new("git").arg("add").arg(".").exec();
-    println!("Error: method git add : {}", err);
-    git_commit();
-}
+    warn!("Error: method git add : {}", err);
 
-fn git_commit() {
     println!("Git Commit. ");
-    git_push();
-}
-fn git_push() {
     println!("Git Push");
-    //let err = Command::new("git").arg("push").arg("-u").exec();
-    //println!("Error: method git push -u : {}", err)
+    let err = Command::new("git").arg("push").arg("-u").exec();
+    println!("Error: method git push -u : {}", err)
 }
 
 //fn test_echo() {
