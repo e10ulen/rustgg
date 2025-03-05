472b6989857d62937c9dd3ef4e5592f36519d343
e10ulen <slcb.sena@gmail.com>
2025-03-05 13:25:32 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,26 @@
-extern crate log;
-use log::{info, warn};
 use std::os::unix::process::CommandExt;
 use std::process::Command;
 fn main() {
-    info!("Git Add files.");
+    //  git add .
+    println!("git add");
+    git_add();
+    println!("git commit");
+    git_commit();
+    println!("git push");
+    git_push();
+}
+fn git_add() {
     let err = Command::new("git").arg("add").arg(".").exec();
-    warn!("Error: method git add : {}", err);
-
-    println!("Git Commit. ");
-    println!("Git Push");
+    println!("Error: method git add . : {}", err);
+}
+fn git_commit() {
+    //  git commit
+    //  git commit -m "{default_value}"
+}
+fn git_push() {
+    //  git push -u
     let err = Command::new("git").arg("push").arg("-u").exec();
-    println!("Error: method git push -u : {}", err)
+    println!("Error: method git push -u : {}", err);
 }
 
 //fn test_echo() {
